a7dc32a6893c17b261bb604d3b7883bc3a340dca
john7114 <118557328+john7114@users.noreply.github.com>
2023-03-05 01:30:37 +0300

    Возведение в степень чисел А и В

---
+++ task26.py
@@ -0,0 +1,24 @@
+# Задача 26:  Напишите программу, которая на вход
+# принимает два числа A и B, и возводит число А
+# в целую неотрицательную степень B с помощью рекурсии.
+
+# *Пример:*
+
+# A = 3; B = 5 -> 243 (3⁵)
+#     A = 2; B = 3 -> 8
+
+def exponentiation(A, B, count=0, res=1):
+    if count == B:
+        print(res)
+    else:
+        res = res*A
+        count += 1
+        exponentiation(A, B, count, res)
+
+
+A = int(input("Введите число А: "))
+B = int(input("Введите число B: "))
+if B < 0:
+    B = -B
+
+exponentiation(A, B)
